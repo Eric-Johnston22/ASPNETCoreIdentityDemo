846ad95e0a0ce35c2bc5d9ba48de291d56009dc9
Eric-Johnston <eric.mjohnston@protonmail.com>
2024-08-04 13:32:48 -0400

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using ASPNETCoreIdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreIdentityDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        //userManager will hold the UserManager instance
+        private readonly UserManager<IdentityUser> userManager;
+
+        //signInManager will hold the SignInManager instance
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        // Both UserManager and SignInManager services are injected into the AccountController
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterView model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Copy data from RegisterView Model to IdentityUser
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                // Store user data in AspNetUsers database table
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                // If user is successfully creates, sign-in the user using
+                // SignInManager and redirect to index action of HomeController
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                // If there are any errors, add them to the ModelState object
+                // which will be displayed by the validation summary tag helper
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    // Handle Successful Login
+                    return RedirectToAction(nameof(HomeController.Index), "home");
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    // hand 2fa case
+                }
+                if (result.IsLockedOut)
+                {
+                    // hande lockout scenario
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                    return View(model);
+                }
+            }
+
+            // If we got this far, something failed. redisplay form.
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "home");
+        }
+    }
+}
